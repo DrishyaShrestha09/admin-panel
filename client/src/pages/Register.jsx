23214015e41908ac5ff1815199866d423891cfed
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-10-01 00:32:54 +0545

    retriving registration data on form submit

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react"
+
 const Register = () => {
-  return <>
+
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+
+  });
+
+  // handling input values
+  const handleInput= (e) =>{
+    console.log(e);
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({
+      ...user,   //using spred operator
+      [name]:value,
+    })
+  }
+
+  // handling the form submission\
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(user);
+  }
+
+  return (
+  <>
     <section>
       <main>
         <div className="section-registration">
@@ -12,25 +42,62 @@ const Register = () => {
               <h1 className="main-heading mb-3">registration form</h1>
               <br />
 
-              <form >
+              <form onSubmit={handleSubmit} >
                 <div>
                   <label htmlFor="username">username</label>
-                  <input type="text" name="username" placeholder="John Doe" id="username" required autoComplete="off" />
+                  <input type="text" 
+                  name="username" 
+                  placeholder="John Doe" 
+                  id="username" 
+                  required 
+                  autoComplete="off"
+                  value={user.username}
+                  onChange={handleInput}
+                  
+                  />
                 </div>
+
                 <div>
                   <label htmlFor="email">email</label>
-                  <input type="email" name="email" placeholder="johndoe@example.com" id="email" required autoComplete="off" />
+                  <input type="email" 
+                  name="email" 
+                  placeholder="johndoe@example.com" 
+                  id="email" 
+                  required 
+                  autoComplete="off" 
+                  value={user.email}
+                  onChange={handleInput}
+                  />
                 </div>
+
                 <div>
                   <label htmlFor="phone">phone</label>
-                  <input type="number" name="phone" placeholder="phone" id="phone" required autoComplete="off" />
+                  <input type="number" 
+                  name="phone" 
+                  placeholder="phone" 
+                  id="phone" 
+                  required 
+                  autoComplete="off" 
+                  value={user.phone}
+                  onChange={handleInput}
+                  />
                 </div>
+
                 <div>
                   <label htmlFor="password">password</label>
-                  <input type="password" name="password" placeholder="password" id="password" required autoComplete="off" />
+                  <input type="password" 
+                  name="password" 
+                  placeholder="password" 
+                  id="password" 
+                  required 
+                  autoComplete="off" 
+                  value={user.password}
+                  onChange={handleInput}
+                  />
                 </div>
-                  <br />
-                  <button type="submit" className="btn btn-submit">Register Now</button>
+
+                <br />
+                <button type="submit" className="btn btn-submit">Register Now</button>
               </form>
             </div>
           </div>
@@ -38,6 +105,7 @@ const Register = () => {
       </main>
     </section>
   </>
+  );
 }
 
 export default Register

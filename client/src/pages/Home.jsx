7b70aecbfc9c8d80e19059fb02aa45dd3e8c9c73
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-10-01 22:25:38 +0545

    added styling in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,38 @@
 const Home = () => {
   return (
-    <div>
-      <h>This is home page </h>
-    </div>
+    <>
+    <main>
+      <section className="section-hero">
+        <div className="container grid grid-two-cols">
+          <div className="hero-content">
+            <p>We are the best crowd funding company</p>
+            <h1>Welcome to Sahayatri</h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere magnam, quos sequi, dolorem dignissimos nobis eius rerum illo debitis nam suscipit tempora eaque facilis.
+            </p>
+            <div className="btn btn-group">
+              <a href="/contact">
+              <button className="btn">connect now</button>
+              </a>
+              <a href="/services">
+                <button className="btn">learn more</button>
+              </a>
+            </div>
+          </div>
+
+          {/* hero image */}
+
+          <div className="hero-image">
+            <img src="/images/home.png" alt="working together" 
+            width="400"
+            height="500"
+            />
+          </div>
+
+        </div>
+      </section>
+    </main>
+    </>
   )
 }
 

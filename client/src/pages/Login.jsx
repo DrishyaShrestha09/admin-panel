350b88dfe23556d8691f574bab334bfde0e31921
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-10-01 19:58:13 +0545

    added login page with data storation in backend

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,79 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [user, setUser] = useState({
+    email:"",
+    pasword: "",
+  });
+
+  const handleInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({
+      ...user,
+      [name]:value,
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  }
+  
+  
+
   return (
-    <div>
-      <h1>This is Login page</h1>
-    </div>
+    <>
+    <section>
+      <main>
+        <div className="section-registration">
+          <div className="container grid grid-two-cols">
+            <div className="login-image">
+              <img src="/images/login.png" alt="registration image" width="500" height="500" />
+            </div>
+            {/* register form */}
+            <div className="registration-form">
+              <h1 className="main-heading mb-3">registration form</h1>
+              <br />
+
+              <form onSubmit={handleSubmit} >
+
+                <div>
+                  <label htmlFor="email">email</label>
+                  <input type="email" 
+                  name="email" 
+                  placeholder="johndoe@example.com" 
+                  id="email" 
+                  required 
+                  autoComplete="off" 
+                  value={user.email}
+                  onChange={handleInput}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="password">password</label>
+                  <input type="password" 
+                  name="password" 
+                  placeholder="password" 
+                  id="password" 
+                  required 
+                  autoComplete="off" 
+                  value={user.password}
+                  onChange={handleInput}
+                  />
+                </div>
+
+                <br />
+                <button type="submit" className="btn btn-submit">Register Now</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </main>
+    </section>
+  </>
   )
 }
 

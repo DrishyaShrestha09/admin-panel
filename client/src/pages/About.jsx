859d4c73f2e3301de9878990f40d42cffeeb3ff1
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-10-02 22:34:28 +0545

    added about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,45 @@
 const About = () => {
   return (
-    <div>
-      <h1>Thsi is about page </h1>
-    </div>
-  )
+    <>
+      <section className="section-hero">
+        <div className="container grid grid-two-cols">
+          <p>Welcome to Sahayatri</p>
+          <h1 className="about-title">Why Choose Us?</h1>
+          <div className="texts">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure deleniti illum vero corporis impedit molestiae
+            <br />
+            <br />
+            recusandae qui blanditiis, ea, doloribus eligendi. Adipisci facilis sed dolore, dicta voluptas unde maxime tempora.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quam pariatur neque maxime magnam ut, ex voluptas nisi
+            <br />
+            <br />
+            sequi ea possimus incidunt saepe amet nihil dolore cum et vel modi Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            <br />
+            <br />
+            Sequi consequuntur delectus, quod aperiam ad tenetur ut, quo veniam, iure ea distinctio atque! Deserunt
+          </p>
+          </div>
+          <div className="btn btn-croup">
+            <a href="/contact">
+              <button className="btn">connect now</button>
+            </a>
+            <a href="/services">
+              <button className="btn secondary-btn">learn more</button>
+            </a>
+          </div>
+          <div className="hero-image">
+              <img src="/images/home.png" alt="working together"
+                width="400"
+                height="500"
+              />
+            </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default About
+export default About;

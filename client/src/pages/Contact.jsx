744d9750b4b909b85e0abd0b430b3bc3386989e8
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-10-03 20:33:08 +0545

    completed ui of contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -2,9 +2,56 @@
 
 const Contact = () => {
   return (
-    <div>
-      <h1>This is contact page</h1>
-    </div>
+    <>
+    <section className="section-contact">
+      <div className="contact-content container">
+        <h1 className="main-heading">Contact us</h1>
+      </div>
+
+      {/* main c0ntact page */}
+      <div className="container grid grid-two-cols">
+        <div className="contact-img">
+          <img src="/images/support.png" alt="image" />
+        </div>
+
+        {/* actual contact content */}
+        <section className="section-form">
+          <form >
+            <div>
+              <label htmlFor="username">username</label>
+              <input type="text" 
+              name="username" 
+              id="username" 
+              autoComplete="off"
+              required 
+
+              />
+            </div>
+            <div>
+              <label htmlFor="email">email</label>
+              <input type="email" 
+              name="email" 
+              id="email" 
+              autoComplete="off"
+              required 
+              
+              />
+            </div>
+
+            <div>
+              <label htmlFor="message">message</label>
+              <textarea name="message" id="message" cols="30" rows="10"></textarea>
+            </div>
+
+            <div>
+              <button type="submit">submit</button>
+            </div>
+          </form>
+        </section>
+
+      </div>
+    </section>
+    </>
   )
 }
 

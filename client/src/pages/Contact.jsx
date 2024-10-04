8316baf41b7a383089d783291a687965cfee9fa3
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-10-04 15:46:13 +0545

    added map

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,96 +1,103 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const Contact = () => {
+  const [contact, setContact] = useState({
+    username: "",
+    email: "",
+    message: "",
+  });
 
-const[contact, setContact] = useState({
-  username: "",
-  email: "",
-  message: "",
-})
+  // handle input 
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
 
-// handle input 
-const handleInput = (e) => {
-  const name = e.target.name;
-  const value = e.target.value;
+    setContact({
+      ...contact,
+      [name]: value,
+    });
+  };
 
-  setContact({
-    ...contact,
-    [name]: value,
-  })
-};
-
-// handeling submit
-const handleSubmit = (e) => {
-  e.preventDefault();
-
-  console.log(contact);
-};
+  // handling submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(contact);
+  };
 
   return (
     <>
-    <section className="section-contact">
-      <div className="contact-content container">
-        <h1 className="main-heading">Contact us</h1>
-      </div>
-
-      {/* main c0ntact page */}
-      <div className="container grid grid-two-cols">
-        <div className="contact-img">
-          <img src="/images/support.png" alt="image" />
+      <section className="section-contact">
+        <div className="contact-content container">
+          <h1 className="main-heading">Contact us</h1>
         </div>
 
-        {/* actual contact content */}
-        <section className="section-form">
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="username">username</label>
-              <input type="text" 
-              name="username" 
-              id="username" 
-              autoComplete="off"
-              value={contact.username}
-              onChange={handleInput}
-              required 
+        {/* main contact page */}
+        <div className="container grid grid-two-cols">
+          <div className="contact-img">
+            <img src="/images/support.png" alt="image" />
+          </div>
 
-              />
-            </div>
-            <div>
-              <label htmlFor="email">email</label>
-              <input type="email" 
-              name="email" 
-              id="email" 
-              autoComplete="off"
-              value={contact.email}
-              onChange={handleInput}
-              required 
-              
-              />
-            </div>
+          {/* actual contact content */}
+          <section className="section-form">
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  autoComplete="off"
+                  value={contact.username}
+                  onChange={handleInput}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  autoComplete="off"
+                  value={contact.email}
+                  onChange={handleInput}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="message">Message</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  autoComplete="off"
+                  value={contact.message}
+                  onChange={handleInput}
+                  required
+                  cols="30"
+                  rows="6"
+                ></textarea>
+              </div>
+              <div>
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+          </section>
+        </div>
 
-            <div>
-              <label htmlFor="message">message</label>
-              <textarea 
-              name="message" 
-              id="message" 
-              autoComplete="off"
-              value={contact.message}
-              onChange={handleInput}
-              required 
-              cols="30" 
-              rows="6"
-              ></textarea>
-            </div>
-
-            <div>
-              <button type="submit">submit</button>
-            </div>
-          </form>
+        <section className="map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56516.31397712411!2d85.3261328!3d27.70896035000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb198a307baabf%3A0xb5137c1bf18db1ea!2sKathmandu%2044600!5e0!3m2!1sen!2snp!4v1728035228972!5m2!1sen!2snp"
+            width="100%"
+            height="450"
+            
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </section>
-
-      </div>
-    </section>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
